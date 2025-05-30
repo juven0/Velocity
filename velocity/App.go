bb42dc35dc3995d87d1c246d75c517d7acf58d46
juven0 <bocquelin@gmail.com>
2025-05-30 16:57:42 +0300

    config project

--- velocity/App.go
+++ velocity/App.go
@@ -1,18 +1,44 @@
 package velocity
 
 import (
+	"github.com/juven0/Velocity/test/router"
 	"github.com/valyala/fasthttp"
 )
 
-type App struct{}
-
-func New() *App {
-	return &App{}
+type App struct {
+	router      *router.Router
+	middlewares []HandlerFunc
 }
 
-func (a *App) Get(path string, handler Hanbler) {
+func New() *App {
+	return &App{
+		router:      router.New(),
+		middlewares: []HandlerFunc{},
+	}
+}
+
+func (a *App) Use(mw HandlerFunc) {
+	a.middlewares = append(a.middlewares, mw)
+}
+
+func (a *App) Get(path string, handler HandlerFunc) {
+	a.router.Handle("GET", path, a.chain(handler))
+}
+
+func (a *App) chain(final HandlerFunc) HandlerFunc {
+	return func(ctx *Context) error {
+		h := final
+		for i := len(a.middlewares) - 1; i >= 0; i-- {
+			next := h
+			mw := a.middlewares[i]
+			h = func(c *Context) error {
+				return mw(c.WithNext(next))
+			}
+		}
+		return h(ctx)
+	}
 }
 
 func (a *App) Listen(addr string) error {
-	return fasthttp.ListenAndServe()
+	return fasthttp.ListenAndServe(addr, a.router.Handler())
 }
