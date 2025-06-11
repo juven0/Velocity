9658170cc25a70d6edea8bed25349a95dc8b2a97
juven0 <bocquelin@gmail.com>
2025-06-11 10:13:22 +0300

    add basic cors middleware

--- middleware/cors/cors.go
+++ middleware/cors/cors.go
@@ -1,16 +1,51 @@
 package cors
 
-import "github.com/juven0/Velocity/types"
+import (
+	"strings"
 
-type Option struct {
+	"github.com/juven0/Velocity/types"
+)
+
+type Config struct {
 	AllowedOrigin       []string
 	AllowedMethode      []string
 	AllowedHeader       []string
 	AllowedCrendentials bool
+	MaxAge              int
 }
 
-func New(option Option) types.HandlerFunc {
-	return func(ctx types.Context) error {
+var DefaultConfig = Config{
+	AllowedOrigin:       []string{"*"},
+	AllowedMethode:      []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTION"},
+	AllowedHeader:       []string{"Content-type", "Authorization", "Accepte"},
+	AllowedCrendentials: false,
+	MaxAge:              3600,
+}
+
+func New(config ...Config) types.HandlerFunc {
+	cfg := DefaultConfig
+
+	if len(config) > 0 {
+		cfg = config[0]
+	}
+
+	AllowAllOrigin := false
+
+	for _, origin := range cfg.AllowedOrigin {
+		if origin == "*" {
+			AllowAllOrigin = true
+		}
+		break
+	}
+
+	if AllowAllOrigin && cfg.AllowedCrendentials {
+		panic("[CORS] Configuration error: When 'AllowCredentials' is set to true, 'AllowOrigins' cannot contain a wildcard origin '*'. Please specify allowed origins explicitly or adjust 'AllowCredentials' setting.")
+	}
+
+	return func(ctx *types.Context) error {
+		ctx.Response.Header.Set("Access-Control-Allow-Origin", strings.Join(cfg.AllowedOrigin, ","))
+		ctx.Response.Header.Set("Access-Control-Allow-Methods", strings.Join(cfg.AllowedMethode, ","))
+		ctx.Response.Header.Set("Access-Control-Allow-Headers", strings.Join(cfg.AllowedHeader, ","))
 		return ctx.Next()
 	}
 }
